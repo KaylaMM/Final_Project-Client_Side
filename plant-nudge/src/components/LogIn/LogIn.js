f916d59592302a7b18e0b80e3dc27ed82a47c85f
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-04-22 22:38:56 -0400

    LogIn Component created

---
+++ plant-nudge/src/components/LogIn/LogIn.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import context, { AuthContext } from "../../context/index";
+
+import NavBar from "../NavBar/NavBar";
+
+import "./LogIn.css";
+
+class LogIn extends Component {
+  render() {
+    return (
+      <AuthContext.Consumer>
+        {(context) => {
+          const {
+            formLogin: { username, password },
+            errorMessage,
+            successMessage,
+            isLoggedIn,
+          } = context.state;
+
+          const { handleLoginInput, handleLoginSubmit } = context;
+          return (
+            <>
+              {isLoggedIn ? (
+                <Redirect to="/user-profile" />
+              ) : (
+                <>
+                  <NavBar />
+                  <h2>Log In</h2>
+                  <form onSubmit={handleLoginInput}>
+                    <label htmlFor="username">
+                      Username:
+                      <input
+                        id="username"
+                        name="username"
+                        type="text"
+                        value={username}
+                        onChange={handleLoginInput}
+                      />
+                    </label>
+                    <label htmlFor="email">
+                      Email:
+                      <input
+                        id="email"
+                        name="email"
+                        type="email"
+                        value={email}
+                        onChange={handleLoginInput}
+                      />
+                    </label>
+                    <label htmlFor="password">
+                      Password:
+                      <input
+                        id="password"
+                        name="password"
+                        type="password"
+                        value={password}
+                        onChange={handleLoginInput}
+                      />
+                    </label>
+                    <button>Sign Up</button>
+                  </form>
+                  {message && <div>{message}</div>}
+                </>
+              )}
+            </>
+          );
+        }}
+      </AuthContext.Consumer>
+    );
+  }
+}
+
+export default LogIn;
