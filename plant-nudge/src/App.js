e3ac84e2ca76d0294e3daba560600c7d8102d80f
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-04-25 01:26:42 -0400

    commit updates to connecting back end to the front end

--- plant-nudge/src/App.js
+++ plant-nudge/src/App.js
@@ -13,19 +13,37 @@ import SignUp from "./components/SignUp/SignUp";
 import UserProfile from "./components/UserProfile/UserProfile";
 
 import { AuthContext } from "./context/index";
+import { ThemeProvider } from "@material-ui/core";
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      iDontKnowWhatGoesHere: [],
+      currentlyLoggedIn: null,
+      //what does ready mean? Liek ready for new session?
+      ready: false,
     };
+
+    this.service = new AuthService();
   }
 
+  getCurrentlyLoggedInUser = (f) => {
+    this.service
+      .currentUser()
+      .then((theUser) => {
+        this.setState({ currentlyLoggedIn: theUser }, () => {
+          if (f) {
+            f();
+          }
+        });
+      })
+      .catch(() => {
+        this.setState({ currentlyLoggedIn: null });
+      });
+  };
+
   componentDidMount() {
-    axios.get("API-LinkHere").then((response) => {
-      this.setState({ iDontKnowWhatGoesHere: response.data });
-    });
+    this.getCurrentlyLoggedInUser();
   }
 
   render() {
