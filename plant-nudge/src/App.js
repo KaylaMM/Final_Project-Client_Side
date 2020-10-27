2c4efdfbeb3f503771fe5f52cbcd1764fadd737e
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-10-26 23:24:44 -0400

    added context and authcontext back to app.js and user profile

--- plant-nudge/src/App.js
+++ plant-nudge/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { AuthContext } from "./context";
+import { AuthContext } from './context/index'
 import "./App.css";
 
 import Home from "./components/Home/Home";
@@ -10,10 +10,11 @@ import SignUp from "./components/SignUp/SignUp";
 import UserProfile from "./components/UserProfile/UserProfile";
 
 function App() {
-  let context = React.useContext(AuthContext);
-  React.useEffect(() => {
-    context.isUserLoggedIn();
-  }, []);
+ let context = React.useContext(AuthContext);
+ 
+ React.useEffect(() => {
+   context.isUserLoggedIn();
+ })
 
   return (
     <div>
--- plant-nudge/src/components/UserProfile/UserProfile.js
+++ plant-nudge/src/components/UserProfile/UserProfile.js
@@ -1,42 +1,45 @@
 import React from "react";
-// import { Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import "./UserProfile.css";
 import NavBar from "../NavBar/NavBar";
 import NewPlant from "../NewPlant/NewPlant";
-// import PlantToggle from "../PlantToggle/PlantToggle";
-// import UploadedPic from "../UploadedPic/UploadedPic";
 import AllPlants from "../AllPlants/AllPlants";
 
-import { AuthContext } from "../../context";
+import { AuthContext } from "../../context/index";
 import "./UserProfile.css";
-// import PlantCard from "../PlantCard/PlantCard";
-
-// make api request to see if user has Avatar, if yes show in href to show it. if not, display default
 
 const UserProfile = () => {
   return (
     <AuthContext.Consumer>
-      <div>
-        <div className="full-user-profile">
-          <div className="user-profile">
-            <NavBar />
-            <div>
-              <img
-                className="ironplanter"
-                src={require("../../Assets/UserProfilePic.jpg")}
-                alt="user-avatar"
-              />
+      {(context) => {
+        const { isLoggedIn } = context.state;
+        return (
+          <div>
+            <div className="full-user-profile">
+              {isLoggedIn ? (
+                <Redirect to="/auth/login" />
+              ) : (
+                <div classNAme="user-profile">
+                  <NavBar />
+                  <div>
+                    <img
+                      className="ironplanter"
+                      src={require("../../Assets/UserProfilePic.jpg")}
+                      alt="user-avatar"
+                    />
+                  </div>
+                  <div className="new-plant-card">
+                    <NewPlant />
+                  </div>
+                </div>
+              )}
             </div>
-            <div className="new-plant-card">
-              <NewPlant />
-            </div>
+            <h1 className="all-plants-header"> Check out your Urban Oasis </h1>
+            <AllPlants />
           </div>
-        </div>
-        <h1 className="all-plants-header"> Check out your Urban Oasis! </h1>
-        <AllPlants />
-      </div>
+        );
+      }}
     </AuthContext.Consumer>
   );
 };
-
 export default UserProfile;
