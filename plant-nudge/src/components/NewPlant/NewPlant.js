02a1cca9e657a6d422cf857b9371a1dc1f95c0cc
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-11-15 23:56:50 -0500

    state updated

--- plant-nudge/src/components/NewPlant/NewPlant.js
+++ plant-nudge/src/components/NewPlant/NewPlant.js
@@ -1,80 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewPlant.css";
-// import NEW_PLANT_SERVICE from "../../services/PlantsService";
 import { AuthContext } from "../../context";
 
+const DEFAULT_PLANT = {
+  plant: "",
+  location: "",
+  waterNeeded: 0,
+};
 
 const NewPlant = () => {
-  
-  constructor() {
-      super();
-  
-      this.state = {
-        newPlantForm: {
-          plant: "",
-          location: "",
-          waterNeeded: "",
-        },
-      };
-    }
+  const [plantInfo, setPlantInfo] = useState(DEFAULT_PLANT);
+
   return (
-    <AuthContext.Consumer>
-      {(context) => {
-        const {
-          newPlantForm: { plant, location, waterNeeded },
-        } = context.state;
-
-        const { handleNewPlantInput, handleNewPlantSubmit } = context;
-
-        return (
-          <div className="createPlant">
-            <div>
-              <form className="new-plant-form">
-                <div>
-                  <i>Plant Type</i>
-                  <input
-                    id="plant"
-                    name="plant"
-                    type="text"
-                    value={plant}
-                    onChange={handleNewPlantInput}
-                    className="validate"
-                  />
-                </div>
-
-                <div className="">
-                  <i id="" className="">
-                    Plant Location
-                  </i>
-                  <input
-                    id="location"
-                    name="location"
-                    type="location"
-                    value={location}
-                    onChange={handleNewPlantInput}
-                    className="validate"
-                  />
-                </div>
-
-                <div className="">
-                  <i id="" className="">
-                    Amount of H20 Needed
-                  </i>
-                  <input id="waterNeeded"
-                    name="waterNeeded"
-                    type="waterNeeded"
-                    value={waterNeeded}
-                    onChange={handleNewPlantInput}
-                    className="validate" />
-                </div>
-              </form>
-
-              <button type="submit" onChange={handleNewPlantSubmit}> Submit </button>
-            </div>
+    <div className="createPlant">
+      <div>
+        <form className="new-plant-form">
+          <div>
+            <i>Plant Type</i>
+            <input
+              id="plant"
+              name="plant"
+              type="text"
+              onChange={plantInfo}
+              className="validate"
+            />
           </div>
-        );
-      }}
-    </AuthContext.Consumer>
+
+          <div className="">
+            <i id="" className="">
+              Plant Location
+            </i>
+            <input
+              id="location"
+              name="location"
+              type="location"
+              onChange={plantInfo}
+              className="validate"
+            />
+          </div>
+
+          <div className="">
+            <i id="" className="">
+              Amount of H20 Needed
+            </i>
+            <input
+              id="waterNeeded"
+              name="waterNeeded"
+              type="waterNeeded"
+              onChange={plantInfo}
+              className="validate"
+            />
+          </div>
+        </form>
+
+        <button type="submit" onChange={setPlantInfo}>
+          {" "}
+          Submit{" "}
+        </button>
+      </div>
+    </div>
   );
 };
 
